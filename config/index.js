fbda4389a8c69ba377bc96f748801a361fd6a098
Sergey Vikhristyuk <s.vikhristyuk@gmail.com>
2014-10-27 03:42:34 +0500

    Mongoose settings added.

---
+++ config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db: 'mongodb://admin:a9363910@ds035250.mongolab.com:35250/uzmarkets'
+}
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var config = require('./config');
+
 var express = require('express');
 var mongoose = require('mongoose');
 
@@ -11,3 +13,18 @@ require('./config/routes')(app);
 
 app.listen(3000);
 console.log('Express app started on port 3000');
+
+// Connect to mongodb
+mongoose.connect(config.db, {
+    server: {
+        socketOptions: {
+            keepAlive: 1
+        }
+    }
+});
+mongoose.connection.on('connected', function () {
+    console.log('Connected to database')
+});
+mongoose.connection.on('error', function (err) {
+    console.log(err);
+});
